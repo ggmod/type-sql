f4e467d23551e2bcb495c58e51c0cc3829a1c18f
ggmod <ggmod64@gmail.com>
2016-10-23 02:23:38 +0200

    type tests fix

--- test/other/type.ts
+++ test/other/type.ts
@@ -4,7 +4,8 @@ import { db } from '../utils';
 
 
 // "select"" return types:
-let q1 = db.from(BOOK).select(BOOK.id, BOOK.title);    // any[]
-let q2 = db.from(BOOK).select();            // Book[]
-let q3 = db.from(BOOK).select(BOOK.title);  // string[]
+let q1 = db.from(BOOK).select();            // Book[]
+let q2 = db.from(BOOK).select(BOOK.title);  // string[]
+let q3 = db.from(BOOK).select(BOOK.id, BOOK.title);    // any[]
 let q4 = db.from(BOOK).select(BOOK.$all);   // Book[]
+let q5 = db.from(BOOK).select(BOOK.$all.count());   // number[]
